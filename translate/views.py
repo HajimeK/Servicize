e274336a7978a301048b3aec66baf9a72dba3801
unknown <kawatah@PC781.qunie.local>
2015-11-18 11:18:52 +0900

    Resolved syntax errors in python. Still found errors below
    Using the URLconf defined in Servicize2.urls, Django tried these URL patterns, in this order:
    ^chat/
    ^iot/
    ^translate/
    ^three/
    ^vr/
    The current URL, , didn't match any of these.

--- translate/views.py
+++ translate/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render_to_response
 import datetime
 
 def home(request):
-  return render_to_response('chat/index.html', { 'clock': datetime.datetime.now()},
+  return render_to_response('translate/index.html', { 'clock': datetime.datetime.now()},)
--- vr/views.py
+++ vr/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render_to_response
 import datetime
 
 def home(request):
-  return render_to_response('vr/index.html', { 'clock': datetime.datetime.now()},
+  return render_to_response('vr/index.html', { 'clock': datetime.datetime.now()},)
