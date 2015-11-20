ea6aabe9516510a00c93862575570a4b23b6d98a
HajimeK <smec.kawata@gmail.com>
2015-11-21 01:10:56 +0900

    Activated three.js with a sample downloaded from GitHub

--- three/views.py
+++ three/views.py
@@ -1,6 +1,5 @@
 # Create your views here.
 from django.shortcuts import render_to_response
-import datetime
 
 def home(request):
-  return render_to_response('three/index.html', { 'clock': datetime.datetime.now()},)
+  return render_to_response('three/index.html',)
